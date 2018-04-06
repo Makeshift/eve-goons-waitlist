c062ee6fa3139dba388860f2606e567bab5a4e48
Samuel Grant <samueljegrant@outlook.com>
2018-04-06 18:37:18 +1200

    Removed 'Defer' from fontawesome

--- template.js
+++ template.js
@@ -69,7 +69,7 @@ function header(headerPayload, cb) {
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
     <!-- Font Awesome 5 -->
-    <script defer src="/includes/vendor/fontawesome/js/fontawesome-all.js"></script>
+    <script src="/includes/vendor/fontawesome/js/fontawesome-all.js"></script>
   </head>
   <body>
     <!-- Header Navbar -->
