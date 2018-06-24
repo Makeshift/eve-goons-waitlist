1d8a40d1ce56c8fd5a06932978dc6cf9d93ba0d1
Mark Lohstroh <mlohstroh@gmail.com>
2018-06-23 20:32:59 -0500

    Adding the index.js for the scheduler. Maybe it's better as an index.js file under the scheduler directory

---
+++ scheduler_index.js
@@ -0,0 +1,15 @@
+import Scheduler from './scheduler/Scheduler';
+
+let scheduler = new Scheduler(1000);
+
+scheduler.every("Test", 10, () => {
+    console.log("I'm doing something important right now");
+});
+
+
+// Run at 8:14 every day.
+scheduler.scheduled(20, 14, "Scheduled Job", () => {
+    console.log("the scheduled job ran!");
+});
+
+scheduler.process();
