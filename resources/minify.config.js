3266b1740b79b206bd356586989f4bb84d658297
Samuel Grant <samueljegrant@outlook.com>
2018-07-18 12:54:46 +1200

    App javascript minifier
    Type `npm run build:js` to compile the JS.

---
+++ resources/minify.config.js
@@ -0,0 +1,46 @@
+
+const log = require('../logger.js')(module);
+
+getPreamble(function(preamble){
+    var jsFiles = ['bird-alert.min.js', 'front.js', 'waitlist-app.js'].map(function (file) {
+        return require('fs').readFileSync('resources/js/' + file, 'utf8');
+    })
+
+    minifyJSGo(jsFiles, function(err){
+        if(err) {
+            log.warn("Failed to minify JS");
+            log.error(err);
+        } else {
+            log.info("Javascript successfully minfied to ../public/waitlist-app.min.js");
+        }
+    });
+
+    function minifyJSGo(code, err){
+        var result =  require('uglify-js').minify(code);
+        
+        //Shit Broke
+        if(result.error) {
+            log.warn("Failed to minify ", result.error)
+            err(result.error);
+            return;
+        }
+        
+        //Write the file
+        require('fs').writeFile("public/includes/waitlist-app.min.js", preamble + result.code, function (error) {
+            err(error)
+        });
+    }
+});
+
+function getPreamble(cb){
+    var text;
+    let d = new Date();
+    text = "/*\n";
+    text += "* Goon Incursion Waitlist\n";
+    text += "* @Author: Samuel Grant\n";
+    text += "* @Repo: https://github.com/makeshift-eve-goons-waitlist\n"
+    text += "* @Includes: Bird-AlertJS, FrontJS\n";
+    text += "* @Updated: " + d.toISOString();
+    text += "\n*/\n";
+    cb(text);
+}
