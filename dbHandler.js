9d963058734024ba99dfdb1755881ca7e084c53a
Samuel Grant <samueljegrant@outlook.com>
2018-09-23 18:20:25 +1200

    Adjusted indentation of the db handler

--- dbHandler.js
+++ dbHandler.js
@@ -2,29 +2,29 @@ const setup = require('./setup.js');
 const mongo = require('mongodb').MongoClient;
 const log = require('./logger.js')(module);
 
-const MONGODB_URL = setup.data.mongoDbURL || process.env.MONGODB_URL ||'mongodb://localhost:27017';
+const MONGODB_URL = setup.data.mongoDbURL || process.env.MONGODB_URL || 'mongodb://localhost:27017';
 const MONGO_DB = setup.data.mongoDbName || process.env.MONOGO_DB;
 
 const absorbInitialConnectError = function absorbInitialConnectError(cb, database) {
-  mongo.connect(MONGODB_URL, function (err, client) {
-		if(err) {
-			log.error('Database connection failure', {err});
-			if(err.message && err.message.match(/failed to connect to server .* on first connect/)) {
-                setTimeout(absorbInitialConnectError.bind(null, cb, dbService), 2000);
-            }
+	mongo.connect(MONGODB_URL, function (err, client) {
+		if (err) {
+			log.error('Database connection failure', { err });
+			if (err.message && err.message.match(/failed to connect to server .* on first connect/)) {
+				setTimeout(absorbInitialConnectError.bind(null, cb, dbService), 2000);
+			}
 		} else {
-            if(database !== undefined) {
-                database.db = client.db(MONGO_DB);
+			if (database !== undefined) {
+				database.db = client.db(MONGO_DB);
 			} else {
-                dbService.db = client.db(MONGO_DB);
-            }
-            
+				dbService.db = client.db(MONGO_DB);
+			}
+
 			log.info("Database connection successful.");
 			cb();
 		}
 	});
 };
 
-let dbService = {connect: absorbInitialConnectError};
+let dbService = { connect: absorbInitialConnectError };
 
 module.exports = dbService;
